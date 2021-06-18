c4014683813c7d13d856499d6ca8f0821c73564e
Emanuel Gallitelli <emanuelgallitelli@gmail.com>
2021-06-17 22:06:26 -0300

    modificado

--- js/promociones.js
+++ js/promociones.js
@@ -16,16 +16,17 @@ function primerTitulo(){
 
 function cambiarPrecio(){
     let precio = document.getElementsByTagName("button");
-    precio[1].innerText="$168.481";
-    precio[2].innerText="$278.312";
-    precio[3].innerText="$99.324";
-    precio[4].innerText="$98.541";
-    precio[5].innerText="$174.119";
-    precio[6].innerText="$110.245";
-    precio[7].innerText="$157.874";
+    precio[2].innerText="$168.481";
+    precio[3].innerText="$278.312";
+    precio[4].innerText="$99.324";
+    precio[5].innerText="$98.541";
+    precio[6].innerText="$174.119";
+    precio[7].innerText="$110.245";
     precio[8].innerText="$92.408";
     precio[9].innerText="$119.684";
     precio[10].innerText="$54.410";
     precio[11].innerText="$182.141";
+    precio[12].innerText="$249.510";
+    precio[13].innerText="$84.684";
 }
 
